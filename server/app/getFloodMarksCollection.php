cf03644c02e89ddc19072c274235f169d518ba48
mkaniaa <mkaniaa@gmail.com>
2020-08-25 22:36:57 +0200

    update content type php

--- server/app/getFloodMarksCollection.php
+++ server/app/getFloodMarksCollection.php
@@ -3,6 +3,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, X-Requested-With');
+header('Content-type:application/json;charset=utf-8');
 
 require './GeoPackage.php';
 
